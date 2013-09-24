b72069a56702c95814cf92df220f7893040aa51a
Apprentice <apprentice@dbc19.local>
2013-09-24 14:05:35 -0700

    js

---
+++ post-it.js
@@ -0,0 +1,45 @@
+function PostIt(x, y){
+  this.x = x
+  this.y = y
+  this.html =  '<div class="post-it", style="left:' + this.x +'; top:' + this.y + '"><div class="header"><div class="x">x</div></div><div class="content"><div contenteditable="true">Type here...</div></div></div>'
+}
+
+var Board = function( selector ) {
+  this.background = $(selector)
+  }
+
+Board.prototype = {
+
+  stopProp: function(){
+    $(this.background).on('click', '.post-it', function(e) {
+      e.stopPropagation();
+    })
+  },
+  postCreate: function(){
+    $(this.background).on('click', function(e){
+   
+      var singlePostit = new PostIt(e.pageX, e.pageY)
+      $(e.target).append(singlePostit.html)
+      $('.post-it').draggable({containment: "parent", handle: ".header"})
+    });
+    this.stopProp()
+  },
+  postRemove: function(){
+    this.postCursor();
+    $('body').on('click', '.x', function(){ 
+      ($(this).closest('.post-it')).remove()
+    })
+  },
+  postCursor: function(){
+    $('body').on('mouseover', '.x', function(){
+      $(this).css('cursor', 'pointer')
+    })
+  }
+}
+
+var board = new Board('#board')
+
+$(function() {
+  board.postCreate()
+  board.postRemove()
+})
